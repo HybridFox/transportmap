639acaec3a52bc7fe1eb1b94d4af5127cccd5479
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2023-05-08 20:08:05 +0200

    feat: add sentry tracing

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,16 +5,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-
-	Sentry.init({
-		dsn: process.env.SENTRY_DSN,
-		tracesSampleRate: 1.0
-	});
-
+	
 	const globalPrefix = 'api';
 	app.setGlobalPrefix(globalPrefix);
 	const port = process.env.PORT || 3000;
 	await app.listen(port);
+
+	Sentry.init({
+		dsn: process.env.SENTRY_DSN,
+		tracesSampleRate: 1.0,
+		integrations: [
+			// enable HTTP calls tracing
+			new Sentry.Integrations.Http({ tracing: true })
+		],
+	});
 
 	console.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
