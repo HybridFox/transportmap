389d9f49b037fd71c2825a0ca612cf9ef9e486f8
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2023-05-16 21:02:24 +0200

    fix: rollback coordinates

--- apps/transportmap/src/components/Head.tsx
+++ apps/transportmap/src/components/Head.tsx
@@ -1,12 +1,15 @@
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import {Helmet} from "react-helmet";
+import { useNavigation } from "react-router-dom";
 
 export const Head: FC = () => {
 	const [t, i18n] = useTranslation();
+	const route = useNavigation();
 
 	return (<Helmet>
 		<meta http-equiv="content-language" content={i18n.language} />
 		<meta name="description" content={t('SEO.META.DESCRIPTION')!} />
+		<link rel="canonical" href={`https://waarismijntrein.be${window.location.pathname}`} />
 	</Helmet>)
 }
