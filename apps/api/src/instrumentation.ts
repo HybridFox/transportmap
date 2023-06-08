d552f6e469a4c1f64a678e0b1b8ea41ca24d9494
Pieterjan Van Saet <hybridfox2@gmail.com>
2023-06-08 12:40:45 +0200

    fix: instruments

--- apps/api/src/instrumentation.ts
+++ apps/api/src/instrumentation.ts
@@ -1,7 +1,9 @@
 import opentelemetry from "@opentelemetry/api";
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import { BasicTracerProvider, SimpleSpanProcessor, ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base';
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import { MongoDBInstrumentation } from '@opentelemetry/instrumentation-mongodb';
 import { PgInstrumentation } from '@opentelemetry/instrumentation-pg';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
@@ -16,20 +18,22 @@ const resource = Resource.default().merge(
 	new Resource({
 		[SemanticResourceAttributes.SERVICE_NAME]: 'transportmap-api',
 		[SemanticResourceAttributes.SERVICE_VERSION]: '0.0.0',
+		[SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: process.env.NODE_ENV,
 	}),
 );
 
 registerInstrumentations({
 	instrumentations: [
+		new HttpInstrumentation(),
+		new ExpressInstrumentation(),
+		new NestInstrumentation(),
 		new MongoDBInstrumentation(),
 		new PgInstrumentation(),
-		new ExpressInstrumentation(),
 		new IORedisInstrumentation(),
-		new NestInstrumentation(),
 	],
 });
 
-const traceProvider = new BasicTracerProvider({
+const traceProvider = new NodeTracerProvider({
 	resource,
 });
 
@@ -45,7 +49,7 @@ traceProvider.addSpanProcessor(
 		}),
 	),
 );
-traceProvider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+// traceProvider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 traceProvider.register();
 
 const metricReader = new PeriodicExportingMetricReader({
