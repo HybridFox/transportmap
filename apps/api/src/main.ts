83135386bb79a6f80494b59499b0058851e5e7ed
Your Name <you@example.com>
2024-01-26 12:58:05 +0100

    fix: undo fixed port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('docs', app, document);
 
-	const port = 3001;
+	const port = process.env.PORT || 3000;
 	await app.listen(port);
 
 	// Sentry.init({
